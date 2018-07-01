47ec50c1f94a24de907eb5e9eb144bc5dc22632b
Damian Pokorski <damianpokorski@hotmail.com>
2018-07-01 17:40:17 +0100

    Minor content update

--- src/vues/About.vue
+++ src/vues/About.vue
@@ -15,16 +15,25 @@
                 So yeah, I'm a developer.
             </p>
 
-            <p style="text-align: justify">
-                Currently based in Blackpool. I have recently graduated Blackpool and the Fylde College (which is the subsidiary of well-esteemed Lancaster University).
-                <br> Right now I am happily employed at Voiteq as a full-time Software Engineer, where I am responsible for a few of their very complicated (and sometimes mind boggling) projects. Doing my best and handling issues on
-                one by one case appears to be the best approach.
+            <p>
+                Recently (May 2018) have moved a Manchester in order to enhance my career progression, as well as to change scenery.
             </p>
+
+            <p style="text-align: justify">
+                 Right now I am happily employed at Moneta Communications Ltd. as a full-time Developer, where I am responsible for a few of their very complicated (and sometimes mind boggling) projects. Doing my best and handling issues on
+                one by one case appears to be the best approach.
+                <br>
+                I have graduated Blackpool and the Fylde College (which is the subsidiary of well-esteemed Lancaster University) with the first class honours degree which I am extremely proud of as only two of those have been awarded within my class.
+            </p>
+            <h5>Professional approach</h5>
+            <p>I like to think of myself as an all-rounder when it comes to Software Engineering, due to my versatile career path which allowed me to experience a lot of different environments and programming stack. If you need someone to build you a responsive website, make you a game, swifly create you a mobile app, manage your database, or establishing a deployment pipeline - I am your guy.</p>
 
             <h5>Background</h5>
             <p style="text-align: justify">
-                Born in Poland, but I've moved to the UK at a tender age of 16 when I have decided to pursue a career related to engineering.
-                <br> After the initial struggle with the language barrier, I have realized that I am quite capable in the field and it is possible for me to stand on top of my peers. Which happened after few months of my Level 2 course.
+                Born in Poland, I've moved to the UK at a tender age of 16 when I have decided to pursue a career related to software engineering.
+
+                <br> After the initial struggle with the language barrier, I have realized that I am quite capable in the field and it is possible for me to stand toe to toe with best of my peers (I'm still quite smug about it). 
+            </p><p>
                 Since then I have achieved the best marks in my class for 6 consecutive years throughout all of the courses that I have attended (Level 2, Extended Level 3 Diploma, Foundation Degree &amp; Honours Degree), which is
                 one of my proudest achievements to this date.
             </p>
@@ -38,7 +47,7 @@
             </p>
         </div>
 
-        <div class="col-md-4 col-sm-12">
+        <div class="col-md-4 col-sm-12 offset-1">
             <h1>&nbsp;</h1>
             <img class="img-fluid" src="/static/img/me.jpg" />
             <p class="text-center" style="color:white;">Me</p>
